55556f2e39af28346c3372118f6ec4e27759011a
Usman Awan <uawan8154@gmail.com>
2024-07-26 08:21:47 +0100

    .

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoDbURL = "mongodb://localhost:27017/todo-list";
+const mongoDbURL = "mongodb+srv://izzakhan8357:maniking@clustertodolist.lwl18sb.mongodb.net/todo-list-backend?retryWrites=true&w=majority&appName=ClusterTodoList";
 
 const mongoConnection = () => {
     mongoose.connect(mongoDbURL)
