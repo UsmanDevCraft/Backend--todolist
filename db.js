3ed292e95fbb2715e008329ea18d4d1e35cac1ff
Usman Awan <uawan8154@gmail.com>
2024-07-27 11:37:29 +0100

    .

--- db.js
+++ db.js
@@ -3,7 +3,19 @@ const mongoose = require("mongoose");
 const mongoDbURL = "mongodb+srv://izzakhan8357:maniking@clustertodolist.lwl18sb.mongodb.net/todo-list-backend?retryWrites=true&w=majority&appName=ClusterTodoList";
 
 const mongoConnection = () => {
-    mongoose.connect(mongoDbURL)
+    mongoose.connect(mongoDbURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        ssl: true,
+        writeConcern: {
+          w: 'majority',
+          wtimeout: 2500
+        }
+      }).then(() => {
+        console.log('Connected to MongoDB');
+      }).catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+      });
 }
 
 
