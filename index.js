ce7240ae209a270160abcdd88975f62f4884f938
Usman Awan <uawan8154@gmail.com>
2024-08-01 06:43:28 +0100

    .

--- index.js
+++ index.js
@@ -7,20 +7,21 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 // Apply CORS middleware globally
-app.use(cors({
-    origin: 'https://todo-list-taupe-seven-56.vercel.app', // Allow only your frontend origin
-    methods: 'GET,POST,PUT,DELETE,OPTIONS', // Specify allowed methods
-    credentials: true, // If you need to include cookies in requests
-}));
+// app.use(cors({
+//     origin: 'https://todo-list-taupe-seven-56.vercel.app', // Allow only your frontend origin
+//     methods: 'GET,POST,PUT,DELETE,OPTIONS', // Specify allowed methods
+//     credentials: true, // If you need to include cookies in requests
+// }));
 
-// Handle preflight requests
-app.options('*', cors({
-    origin: 'https://todo-list-taupe-seven-56.vercel.app', // Allow only your frontend origin
-    methods: 'GET,POST,PUT,DELETE,OPTIONS', // Specify allowed methods
-    credentials: true, // If you need to include cookies in requests
-}));
+// // Handle preflight requests
+// app.options('*', cors({
+//     origin: 'https://todo-list-taupe-seven-56.vercel.app', // Allow only your frontend origin
+//     methods: 'GET,POST,PUT,DELETE,OPTIONS', // Specify allowed methods
+//     credentials: true, // If you need to include cookies in requests
+// }));
 
 app.use("/api/tasks", require("./routes/Lists"));
 app.use("/api/auth", require("./routes/auth"));
